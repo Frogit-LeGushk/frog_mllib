78b010eddc67fe424d68e689bf16ca18b95ccf44
Byshkov Oleg <byshkov.oleg@yandex.ru>
2025-08-26 14:06:02 +0300

    Migrate method from linear.py

--- general_utils/utils.py
+++ general_utils/utils.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
 
+import numpy as np
 import pandas as pd
 import polars as pl
 import polars_ds as pl_ds
@@ -170,7 +171,33 @@ def plot_lines_and_bins_roc_auc(
             ax_twin.set_ylim(0, df_agg[f'count_{bin_colname}'].max() * scale_top_offset_bins)
         ax_twin.legend(loc='lower center', framealpha=0.4)
 
-
+def plot_weights(model, features, top_k=20):
+    '''
+    рисует значения весов линейной модели при признаках
+    
+    top_k: рисовать первые top_k весов по модулю
+    '''
+    # подготовка необходимого
+    num_features_to_plot = min(top_k, len(features))
+    weights = model.coef_[0]
+    sorted_idx = np.argsort(-np.abs(weights))
+    bias = model.intercept_[0]
+    
+    fig, ax = plt.subplots(figsize=(8, num_features_to_plot / 2))
+    
+    # сами бары
+    container = ax.barh(y=features[sorted_idx][:top_k][::-1], width=weights[sorted_idx][:top_k][::-1])
+    
+    # приписать к ним значения весов
+    ax.bar_label(container, weights[sorted_idx][:top_k][::-1].round(3), color='red', fontsize=15)
+    
+    # настройка ах'a
+    ax.margins(0.2, 0.05)
+    ax.set_title(f'bias: {bias:.1e}', fontsize=15)
+    ax.tick_params(axis='both', labelsize=15)
+    ax.set_xlabel('weight', fontsize=15)
+    
+    plt.show()
 
 def get_df_info(df: pd.DataFrame, thr: float = 0.01, accur: int = 3):
     '''
